9933dedb846508e894260e502e6076df808c23c0
akarshjairaj <akarshjairaj@gmail.com>
2021-10-18 11:25:17 +0530

    ADD storeFactory explanation

--- src/js/redux/storeFactory.js
+++ src/js/redux/storeFactory.js
@@ -23,7 +23,41 @@ const saver = (store) => (next) => (action) => {
 };
 
 // Make storeFactory function
-const storeFactory = () =>
-  applyMiddleware(logger, saver)(createStore)(rootReducer, initialState);
+
+// const storeFactory = () =>
+//   applyMiddleware(logger, saver)(createStore)(rootReducer, initialState);
+
+// const storeFactory = () => {
+//   return applyMiddleware(logger, saver)(createStore)(rootReducer, initialState);
+// };
+
+// const storeFactory = () => {
+//   const f = applyMiddleware(logger, saver)(createStore)(
+//     rootReducer,
+//     initialState
+//   );
+//   return f;
+// };
+
+// const storeFactory = () => {
+//   const a = applyMiddleware(logger, saver);
+//   const f = a(createStore)(rootReducer, initialState);
+//   return f;
+// };
+
+const storeFactory = () => {
+  // applyMiddleware returns a function
+  // this returned function (a) expects createStore as its argument (createStore is a function)
+  const a = applyMiddleware(logger, saver);
+  // a (which is a func returned from applyMiddleware) is invoked with createStore as its argument
+  // this function returns a function (c)
+  const c = a(createStore);
+  // c when invoked with rootReducer and initialState returns the function
+  // which when invoked returns the store with the configured middleware
+  const f = c(rootReducer, initialState);
+  return f;
+};
+
+// const store = createStore(reducer, initialState);
 
 export default storeFactory;
